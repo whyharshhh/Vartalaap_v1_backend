a0072e3742d6f42e4955a313c4498e12d424d387
Harsh Yadav <iyadavharsh261@gmail.com>
2025-06-21 14:52:06 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -30,13 +30,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // ✅ Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 // ✅ Start server
 server.listen(PORT, () => {
